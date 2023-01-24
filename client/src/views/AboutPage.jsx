4f939bae3ad798dd191aecd8f9d39974b7d3247f
Anthony Bosek <anthony.bosek@gmail.com>
2023-01-24 06:03:25 -0700

    aboutPage content

--- client/src/views/AboutPage.jsx
+++ client/src/views/AboutPage.jsx
@@ -1,6 +1,7 @@
 import { Box } from "@mui/material";
 import FlexBetween from "../components/FlexBetween";
 import RegularTxt from "../components/RegularTxt";
+import avatar from "../assets/images/avatar.jpg";
 
 const AboutPage = () => {
   return (
@@ -16,9 +17,57 @@ const AboutPage = () => {
           height="70%"
           width="80%"
           m="2rem auto"
-          gridTemplateColumns="30% 60%"
+          gridTemplateColumns="30% 70%"
         >
-          <Box borderRight="1px solid whitesmoke" paddingTop="1rem"></Box>
+          <Box borderRight="1px solid #333" padding="1rem">
+            <img
+              src={avatar}
+              alt="avatar"
+              width="90%"
+              // style={{ borderRadius: "50%" }}
+            />
+          </Box>
+          <Box sx={{ padding: "1rem" }}>
+            <RegularTxt sx={{ textAlign: "center", fontSize: "1.2rem" }}>
+              I’m Anthony. I’m a web developer and software engineer. I
+              specialize in the <b>MERN</b> stack with an emphasis on logic and
+              functionality. I have a working knowledge of{" "}
+              <b>
+                Redux/ToolKit, Graphql, D3, Nivo, Material-UI, Tailwind,
+                AntDesign,
+              </b>{" "}
+              & more.
+              <Box
+                sx={{
+                  maxWidth: "8vw",
+                  borderBottom: "1px solid dodgerblue",
+                  margin: "1rem auto",
+                }}
+              />
+            </RegularTxt>
+            <ul>
+              <li>
+                <span style={{ color: "#333", paddingRight: "1rem" }}>▶</span>
+                <b>birthday:</b> April 10, 1981
+              </li>
+              <li>
+                <span style={{ color: "#333", paddingRight: "1rem" }}>▶</span>
+                <b>location:</b> Phoenix, AZ
+              </li>
+              <li>
+                <span style={{ color: "#333", paddingRight: "1rem" }}>▶</span>
+                <b>experience:</b> 2+ years
+              </li>
+              <li>
+                <span style={{ color: "#333", paddingRight: "1rem" }}>▶</span>
+                <b>email:</b> anthony.bosek@gmail.com
+              </li>
+              <li>
+                <span style={{ color: "#333", paddingRight: "1rem" }}>▶</span>
+                <b>cell:</b> 480) 236 - 9069
+              </li>
+            </ul>
+          </Box>
         </Box>
       </Box>
     </FlexBetween>
