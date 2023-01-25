764a91f7123ae5a6097c9572cd86a25f38c5206d
Anthony Bosek <anthony.bosek@gmail.com>
2023-01-25 14:08:03 -0700

    skillPage complete

---
+++ client/src/assets/data/language.js
@@ -0,0 +1,152 @@
+import antDesign from "../images/antDesign.png";
+import aws from "../images/aws.png";
+import axios from "../images/axios.png";
+import bs from "../images/bs.png";
+import css from "../images/css.png";
+import d3 from "../images/d3.png";
+import express from "../images/express.png";
+import graphQL from "../images/graphQL.png";
+import html from "../images/html.png";
+import jquery from "../images/jquery.png";
+import js from "../images/js.png";
+import mongodb from "../images/mongodb.png";
+import mongoose from "../images/mongoose.png";
+import mui from "../images/mui.png";
+import nivo from "../images/nivo.png";
+import node from "../images/node.png";
+import npm from "../images/npm.png";
+import react from "../images/react.png";
+import redux from "../images/redux.png";
+import sass from "../images/sass.png";
+import tailwind from "../images/tailwind.png";
+
+export const languageArray = [
+  {
+    header: "front-end",
+    span: "1 / 3",
+    languages: [
+      {
+        src: html,
+        alt: "html",
+      },
+      {
+        src: css,
+        alt: "css",
+      },
+      {
+        src: js,
+        alt: "js",
+      },
+      {
+        src: jquery,
+        alt: "jquery",
+      },
+      {
+        src: react,
+        alt: "react",
+      },
+      {
+        src: redux,
+        alt: "redux",
+      },
+      {
+        src: npm,
+        alt: "npm",
+      },
+    ],
+  },
+  {
+    header: "ui/ux",
+    span: "1 / 3",
+    languages: [
+      {
+        src: sass,
+        alt: "sass",
+      },
+      {
+        src: mui,
+        alt: "mui",
+      },
+      {
+        src: tailwind,
+        alt: "tailwind",
+      },
+      {
+        src: bs,
+        alt: "bs",
+      },
+      {
+        src: antDesign,
+        alt: "antDesign",
+      },
+    ],
+  },
+  {
+    header: "data-vis",
+    span: "1 / 2",
+    languages: [
+      {
+        src: d3,
+        alt: "d3",
+      },
+      {
+        src: nivo,
+        alt: "nivo",
+      },
+    ],
+  },
+  {
+    header: "api",
+    span: "2 / 3",
+    languages: [
+      {
+        src: axios,
+        alt: "axios",
+      },
+      {
+        src: mongoose,
+        alt: "mongoose",
+      },
+      {
+        src: graphQL,
+        alt: "graphQL",
+      },
+    ],
+  },
+  {
+    header: "back-end",
+    span: "1 / 3",
+    languages: [
+      {
+        src: node,
+        alt: "node",
+      },
+      {
+        src: express,
+        alt: "express",
+      },
+      {
+        src: mongodb,
+        alt: "mongodb",
+      },
+
+      {
+        src: aws,
+        alt: "aws",
+      },
+    ],
+  },
+];
+
+export const softSkillsArray = [
+  "communication",
+  "leadership",
+  "time management",
+  "critical thinking",
+  "teamwork",
+  "interpersonal",
+  "adaptability",
+  "problem solving",
+  "organization",
+  "creativity",
+];
